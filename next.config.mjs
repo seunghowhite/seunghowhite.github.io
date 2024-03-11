9e1fe3ffea7f9935a2660c592db207d77b5a2495
seunghobeak <qortmdgh0320@megabridge.co.kr>
2024-03-11 14:27:36 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
+  },
+};
 
 export default nextConfig;
