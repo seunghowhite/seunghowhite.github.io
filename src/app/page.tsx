ca503f7b66e0c18ad3352fd19b4d3db4005491f7
bsh <qortmdgh0320@gmail.com>
2025-09-11 11:50:33 +0900

    FEAT: 홈 페이지에 카테고리 비율 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import CategoryPercentage from "@/components/etc/CategoryPercentage";
 import stonTower from "@/public/home/stone_tower.png";
 
 export default function HomePage() {
@@ -24,51 +25,72 @@ export default function HomePage() {
       <div className="flex max-w-[600px] flex-col items-center gap-4 text-center">
         <h1 className="text-2xl font-bold sm:text-3xl">처음은 다 어렵습니다</h1>
         <p className="break-words text-lg text-gray-600 dark:text-gray-400 sm:text-xl">
-          돌탑 쌓듯 꾸준하고 차근히 어떤 일을 행하면 좋은 결과가 있을 것입니다.
+          돌탑 쌓듯 꾸준히 어떤 일을 행하면 좋은 결과가 있을 것입니다.
         </p>
 
-        <div className="flex flex-col items-start gap-2 text-gray-700 dark:text-gray-300 sm:text-lg">
+        <div className="flex flex-col items-start gap-4 text-gray-700 dark:text-gray-300 sm:text-lg">
           <Link
             href={"/knowledge"}
-            className="underline-offset-4 hover:underline"
+            className="w-full underline-offset-4 hover:underline"
           >
-            <p>
-              <strong
-                className="sm:text-3xl"
-                style={{ color: "#4A9ED4" }}
-              >
-                지:
-              </strong>
-              개발 관련 내용을 담고 있습니다.
-            </p>
+            <div className="flex flex-col gap-2">
+              <p className="text-start">
+                <strong
+                  className="sm:text-3xl"
+                  style={{ color: "#4A9ED4" }}
+                >
+                  지:
+                </strong>
+                개발 관련 내용을 담고 있습니다.
+              </p>
+              <CategoryPercentage
+                subject="knowledge"
+                color="#4A9ED4"
+                label="지"
+              />
+            </div>
           </Link>
           <Link
             href={"/moral"}
-            className="underline-offset-4 hover:underline"
+            className="w-full underline-offset-4 hover:underline"
           >
-            <p>
-              <strong
-                className="sm:text-3xl"
-                style={{ color: "#F5A45D" }}
-              >
-                덕:
-              </strong>
-              일상생활에서 얻는 생각을 기록합니다.
-            </p>
+            <div className="flex flex-col gap-2">
+              <p className="text-start">
+                <strong
+                  className="sm:text-3xl"
+                  style={{ color: "#F5A45D" }}
+                >
+                  덕:
+                </strong>
+                일상생활에서 얻는 생각을 기록합니다.
+              </p>
+              <CategoryPercentage
+                subject="moral"
+                color="#F5A45D"
+                label="덕"
+              />
+            </div>
           </Link>
           <Link
             href={"/body"}
-            className="underline-offset-4 hover:underline"
+            className="w-full underline-offset-4 hover:underline"
           >
-            <p>
-              <strong
-                className="sm:text-3xl"
-                style={{ color: "#E97362" }}
-              >
-                체:
-              </strong>
-              몸을 가꾸는 기록을 담았습니다.
-            </p>
+            <div className="flex flex-col gap-2">
+              <p className="text-start">
+                <strong
+                  className="sm:text-3xl"
+                  style={{ color: "#E97362" }}
+                >
+                  체:
+                </strong>
+                몸을 가꾸는 기록을 담았습니다.
+              </p>
+              <CategoryPercentage
+                subject="body"
+                color="#E97362"
+                label="체"
+              />
+            </div>
           </Link>
         </div>
       </div>
